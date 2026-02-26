314d39dfa90bad68f7d5eac9bfb5f2b280b88dd4
Amir Saman Pirayeshfar <31203866+pirayeshfar@users.noreply.github.com>
2026-02-27 00:27:12 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,18 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+export default defineConfig(({ command }) => ({
+  plugins: [react(), tailwindcss()],
+  // For GitHub Pages: github.com/pirayeshfar/seti-portalgraph
+  base: command === 'build' ? "/seti-portalgraph/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-});
+}));
