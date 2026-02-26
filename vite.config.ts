69149bd5c5dcc48206e36baf518aaadb59e20677
Amir Saman Pirayeshfar <31203866+pirayeshfar@users.noreply.github.com>
2026-02-27 00:48:46 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="refresh" content="0; url=./index.html">
-</head>
-<body>
-<p>Redirecting...</p>
-</body>
-</html>
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ command }) => ({
+  plugins: [react(), tailwindcss()],
+  base: command === 'build' ? "/seti-portalgraph/" : "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+}));
